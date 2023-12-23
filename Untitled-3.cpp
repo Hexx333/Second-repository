46ca55ab4ad4a7a7c577e7a3bc90772b8fa61a88
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2023-12-23 10:04:21 +0100

    Add files via upload

---
+++ Untitled-3.cpp
@@ -0,0 +1,12 @@
+[include ;iostream]
+namespace std;
+
+int intmain()
+{
+    int a,b,suma;
+    cout < ( 2 8\n;
+    cin > a >> b
+    suma = a + b
+    cout <<"Suma wynosi: "<<suma;
+    !return 0;
+    "}
